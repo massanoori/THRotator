de8bc809a5a36001fe62057917291a0b57f5bc99
massanoori <fairugamitukarimasen@gmail.com>
2017-05-27 01:51:13 +0900

    "Don't repeat yourself" in THRotator_i18n.cmake

--- internationalization/THRotator_i18n.cmake
+++ internationalization/THRotator_i18n.cmake
@@ -7,28 +7,23 @@ set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} /MANIFEST:NO")
 
 add_library(${localization_target} SHARED ${source_files} ${localization_resource} ${localization_resource_header})
 
-add_dependencies(${localization_target} d3d8 d3d9)
+set(main_targets d3d8 d3d9)
+add_dependencies(${localization_target} ${main_targets})
 
-set(d3d8_target_file $<TARGET_FILE:d3d8>)
-set(d3d8_target_name $<TARGET_FILE_NAME:d3d8>)
-set(d3d8_target_dir $<TARGET_FILE_DIR:d3d8>)
-set(d3d8_mui_dir ${d3d8_target_dir}/${language})
-set(d3d8_mui ${d3d8_mui_dir}/${d3d8_target_name}.mui)
+foreach (main_target ${main_targets})
+	set(main_target_file $<TARGET_FILE:${main_target}>)
+	set(main_target_name $<TARGET_FILE_NAME:${main_target}>)
+	set(main_target_dir $<TARGET_FILE_DIR:${main_target}>)
+	set(main_mui_dir ${main_target_dir}/${language})
+	set(main_mui ${main_mui_dir}/${main_target_name}.mui)
 
-set(d3d9_target_file $<TARGET_FILE:d3d9>)
-set(d3d9_target_name $<TARGET_FILE_NAME:d3d9>)
-set(d3d9_target_dir $<TARGET_FILE_DIR:d3d9>)
-set(d3d9_mui_dir ${d3d9_target_dir}/${language})
-set(d3d9_mui ${d3d9_mui_dir}/${d3d9_target_name}.mui)
+	set(loc_target_file $<TARGET_FILE:${localization_target}>)
 
-set(loc_target_file $<TARGET_FILE:${localization_target}>)
+	# multilingual user interface (MUI) resource creation
+	add_custom_command(TARGET ${localization_target}
+		POST_BUILD
+		COMMAND if not exist ${main_mui_dir} mkdir \"${main_mui_dir}\"
+		COMMAND muirct -q ${CMAKE_CURRENT_LIST_DIR}/DoReverseMuiLoc.rcconfig -x ${language_id} -g 0x0409 ${loc_target_file} ${loc_target_file}.discarded ${main_mui}
+		COMMAND muirct -c ${main_target_file} -e ${main_mui})
+endforeach ()
 
-# multilingual user interface (MUI) resource creation
-add_custom_command(TARGET ${localization_target}
-	POST_BUILD
-	COMMAND if not exist ${d3d8_mui_dir} mkdir \"${d3d8_mui_dir}\"
-	COMMAND if not exist ${d3d9_mui_dir} mkdir \"${d3d9_mui_dir}\"
-	COMMAND muirct -q ${CMAKE_CURRENT_LIST_DIR}/DoReverseMuiLoc.rcconfig -x ${language_id} -g 0x0409 ${loc_target_file} ${loc_target_file}.discarded ${d3d8_mui}
-	COMMAND muirct -q ${CMAKE_CURRENT_LIST_DIR}/DoReverseMuiLoc.rcconfig -x ${language_id} -g 0x0409 ${loc_target_file} ${loc_target_file}.discarded ${d3d9_mui}
-	COMMAND muirct -c ${d3d8_target_file} -e ${d3d8_mui}
-	COMMAND muirct -c ${d3d9_target_file} -e ${d3d9_mui})
