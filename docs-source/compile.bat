3a659b9aad4c2780103eb73b3538b5d613a6fbc8
massanoori <fairugamitukarimasen@gmail.com>
2017-07-18 23:55:06 +0900

    Conciser output of compile.bat

--- docs-source/compile.bat
+++ docs-source/compile.bat
@@ -2,24 +2,22 @@
 
 setlocal
 
-echo Checking Python executable.
-echo[
-
-python --version
+python --version 2>NUL
 if errorlevel 9009 (
-  echo[
   echo Failed to find Python executable.
   echo Make sure you have installed Python or added Python directory to PATH.
   exit /b 1
 )
 
-echo[
-
 pushd en
+echo ##### Start building English manual #####
 call make html
+echo[
 popd
 
 pushd ja
+echo ##### Start building Japanese manual #####
 call make html
+echo[
 popd
 
