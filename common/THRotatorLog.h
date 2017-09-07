084eeb0307ad2ef1a11a654a59e9f6ec161a9f3d
massanoori <fairugamitukarimasen@gmail.com>
2017-09-07 23:38:20 +0900

    Fix CID 170447 Uncaught exception

--- common/THRotatorLog.h
+++ common/THRotatorLog.h
@@ -15,7 +15,15 @@ void OutputLogMessage(LogSeverity severity, const std::string& message);
 template <typename... ArgsType>
 void OutputLogMessagef(LogSeverity severity, const std::string& format, ArgsType&&... args)
 {
-	OutputLogMessage(severity, fmt::format(format, std::forward<ArgsType>(args)...));
+	try
+	{
+		OutputLogMessage(severity, fmt::format(format, std::forward<ArgsType>(args)...));
+	}
+	catch (const fmt::FormatError&)
+	{
+		OutputLogMessage(LogSeverity::Error, "Failed to parse log string.");
+		OutputLogMessage(LogSeverity::Error,
+			std::string("Original message: '") + format + "'");
+	}
 }
-
 
