7cf53ee965201de1daf942fee5e982fe8a1c98bd
massanoori <fairugamitukarimasen@gmail.com>
2017-09-10 00:17:51 +0900

    Update version to 2.0.1

--- common/THRotatorEditor.cpp
+++ common/THRotatorEditor.cpp
@@ -22,7 +22,7 @@
 namespace
 {
 
-const char* THROTATOR_VERSION_STRING = "2.0.0";
+const char* THROTATOR_VERSION_STRING = "2.0.1";
 
 HHOOK ms_hHook;
 std::map<HWND, std::weak_ptr<THRotatorEditorContext>> ms_touhouWinToContext;
