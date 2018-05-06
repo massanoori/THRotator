a1c1c77a9a565ee15a0c6ced72e28bb6ec8a7d0f
massanoori <fairugamitukarimasen@gmail.com>
2018-05-06 18:53:51 +0900

    Fix compiler warning

--- internationalization/dllmain.cpp
+++ internationalization/dllmain.cpp
@@ -1,11 +1,11 @@
 // dllmain.cpp : DLL アプリケーションのエントリ ポイントを定義します。
 #include "stdafx.h"
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-					 )
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
 {
+	UNREFERENCED_PARAMETER(hModule);
+	UNREFERENCED_PARAMETER(lpReserved);
+
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
